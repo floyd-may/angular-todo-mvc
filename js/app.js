5318891afd59e38b9376ff97c53f649c8781407c
Floyd May <floyd.may@gmail.com>
2015-04-07 20:30:00 -0500

    yay group TDD!

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 angular.module('todo-mvc', []).controller('todos', [
 '$scope',
 function($scope){
+  $scope.addNewTodo = function(){
+    $scope.todos = $scope.todos || [];
+    if ($scope.newTodo) {
+      $scope.todos.push({ title: $scope.newTodo });
+      $scope.newTodo = "";
+    }
+  };
 
+  $scope.$watch('todos', function(){
+    $scope.incompleteCount=$scope.todos.length;
+  }, true);
 }]);
--- test/app-tests.js
+++ test/app-tests.js
@@ -14,8 +14,64 @@ describe('tests for todo-mvc app', function(){
 
   }));
 
-  it('should exist', function(){
-    createController();
-    expect(1).toBe(1);
+  describe('addNewTodo function', function(){
+    describe('with no todos present', function(){
+      beforeEach(function(){
+        createController();
+        $scope.newTodo = "new todo";
+        $scope.addNewTodo();
+      });
+
+      it('should add todo', function(){
+        expect($scope.todos).toEqual([{title:"new todo"}]);
+      });
+
+      it('should clear text from input', function(){
+        expect($scope.newTodo).toEqual("");
+      });
+    });
+
+    it('should save text entered on form submit', function(){
+      createController();
+      $scope.newTodo = "frank joe bob";
+      $scope.addNewTodo();
+      expect($scope.todos).toEqual([{title:"frank joe bob"}]);
+    });
+
+    it('should append text entered on form submit', function(){
+      createController();
+      $scope.todos = [{title:"herp derp"}];
+      $scope.newTodo = "frank joe bob";
+      $scope.addNewTodo();
+      expect($scope.todos).toEqual(
+        [{title:"herp derp"},
+         {title:"frank joe bob"}]);
+    });
+    it('should not append empty todo on form submit', function(){
+      createController();
+      $scope.todos = [{title:"herp derp"}];
+      $scope.newTodo = "";
+      $scope.addNewTodo();
+      expect($scope.todos).toEqual(
+        [{title:"herp derp"}]);
+    });
+
+
+
+
+  });
+  describe('counts',function(){
+    it('should be 1 when a single todo exists',function(){
+      createController();
+      $scope.todos = [{title:"herp derp2"}];
+      $scope.$digest();
+      expect($scope.incompleteCount).toBe(1);
+    });
+    it('should increment by 1 after a second one is added',function(){
+      createController();
+      $scope.todos=[{title:"herp derp2"},{title:"herp derp3"}];
+      $scope.$digest();
+      expect($scope.incompleteCount).toBe(2);
+    });
   });
 });
