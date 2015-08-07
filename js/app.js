dddbc4050b989d8686bd329b7a80a610e4e1a4e0
Floyd May <floyd-may@github>
2015-08-07 11:00:14 -0500

    passing test for uncompleted count

--- js/app.js
+++ js/app.js
@@ -11,4 +11,15 @@ function($scope){
         $scope.newTodo = '';
     };
 
+    $scope.$watch('todos', function(){
+        $scope.uncompletedCount =
+            $scope.todos.reduce(function(agg, val){
+                if(!val.completed){
+                    return agg + 1;
+                }
+
+                return agg;
+            }, 0);
+    }, true);
+
 }]);
